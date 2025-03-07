36f78cc4189db3c5250286b560e006e2d01b86ca
mbc2009 <liuhonglin520@gmail.com>
2025-03-07 03:02:02 -0500

    done

--- ProgramAssignments/PA_2/code/EXE_note.sh
+++ ProgramAssignments/PA_2/code/EXE_note.sh
@@ -2,117 +2,17 @@ clear
 
 # Resources request
 module load openmpi
-salloc -A gts-yw6 -N1 --ntasks-per-node=24 --ntasks-per-core=1 --mem=0 -t00:20:00 --constraint=gold6226
+#salloc -A gts-yw6 -N1 --ntasks-per-node=24 --ntasks-per-core=1 --mem=0 -t00:20:00 --constraint=gold6226
 
 # compile
 make   allreduce 
 make   many2many
 
 # self test
-srun    -n 5 ./m2m ./small_example/m2m_smallinput.txt
-srun    -n 6 ./ard ./small_example/ard_smallinput.txt
+#srun    -n 5 ./m2m ./small_example/m2m_smallinput.txt
+#srun    -n 6 ./ard ./small_example/ard_smallinput.txt
 
 # grading
 bash   autograder_gendata.sh # generate massive (2.5 GB) data for autograder, loc: ./autograderdata (files inside deleted)
 bash  autograder.sh
 
-
-<< COMMENT
-mpicxx -Wall -O3 -std=c++2a -o ard allreduce_main.cpp custom_collectives.cpp
-mpicxx -Wall -O3 -std=c++2a -o m2m manytomany_main.cpp custom_collectives.cpp
-Architecture:             x86_64
-  CPU op-mode(s):         32-bit, 64-bit
-  Address sizes:          46 bits physical, 48 bits virtual
-  Byte Order:             Little Endian
-CPU(s):                   24
-  On-line CPU(s) list:    0-23
-Vendor ID:                GenuineIntel
-  Model name:             Intel(R) Xeon(R) Gold 6226 CPU @ 2.70GHz
-    CPU family:           6
-    Model:                85
-    Thread(s) per core:   1
-    Core(s) per socket:   12
-    Socket(s):            2
-    Stepping:             7
-    CPU(s) scaling MHz:   73%
-    CPU max MHz:          3700.0000
-    CPU min MHz:          1200.0000
-    BogoMIPS:             5400.00
-    Flags:                fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush dts acpi mmx fxsr sse sse2 ss ht tm p
-                          be syscall nx pdpe1gb rdtscp lm constant_tsc art arch_perfmon pebs bts rep_good nopl xtopology nonstop_tsc cpuid aper
-                          fmperf pni pclmulqdq dtes64 monitor ds_cpl vmx smx est tm2 ssse3 sdbg fma cx16 xtpr pdcm pcid dca sse4_1 sse4_2 x2api
-                          c movbe popcnt tsc_deadline_timer aes xsave avx f16c rdrand lahf_lm abm 3dnowprefetch cpuid_fault epb cat_l3 cdp_l3 i
-                          ntel_ppin ssbd mba ibrs ibpb stibp ibrs_enhanced tpr_shadow flexpriority ept vpid ept_ad fsgsbase tsc_adjust bmi1 avx
-                          2 smep bmi2 erms invpcid cqm mpx rdt_a avx512f avx512dq rdseed adx smap clflushopt clwb intel_pt avx512cd avx512bw av
-                          x512vl xsaveopt xsavec xgetbv1 xsaves cqm_llc cqm_occup_llc cqm_mbm_total cqm_mbm_local dtherm ida arat pln pts hwp h
-                          wp_act_window hwp_epp hwp_pkg_req vnmi pku ospke avx512_vnni md_clear flush_l1d arch_capabilities
-Virtualization features:  
-  Virtualization:         VT-x
-Caches (sum of all):      
-  L1d:                    768 KiB (24 instances)
-  L1i:                    768 KiB (24 instances)
-  L2:                     24 MiB (24 instances)
-  L3:                     38.5 MiB (2 instances)
-NUMA:                     
-  NUMA node(s):           2
-  NUMA node0 CPU(s):      0-11
-  NUMA node1 CPU(s):      12-23
-Vulnerabilities:          
-  Gather data sampling:   Vulnerable
-  Itlb multihit:          KVM: Mitigation: VMX disabled
-  L1tf:                   Not affected
-  Mds:                    Not affected
-  Meltdown:               Not affected
-  Mmio stale data:        Vulnerable
-  Reg file data sampling: Not affected
-  Retbleed:               Vulnerable
-  Spec rstack overflow:   Not affected
-  Spec store bypass:      Vulnerable
-  Spectre v1:             Vulnerable: __user pointer sanitization and usercopy barriers only; no swapgs barriers
-  Spectre v2:             Vulnerable; IBPB: disabled; STIBP: disabled; PBRSB-eIBRS: Vulnerable; BHI: Vulnerable
-  Srbds:                  Not affected
-  Tsx async abort:        Mitigation; TSX disabled
--------------------------------------
-rm -f m2m ard
-mpicxx -Wall -O3 -std=c++2a -o m2m manytomany_main.cpp custom_collectives.cpp
-mpicxx -Wall -O3 -std=c++2a -o ard allreduce_main.cpp custom_collectives.cpp
- 
-=================== CHECKING TEST FILE ===================
- 
- 
-=================== CORRECTNESS TESTS ===================
- 
-##AllReduce 15
-Time taken by custom function: 12.765098
-AllReduce P=15 Correctness passed!
- 
-##AllReduce 24
-Time taken by custom function: 22.296647
-AllReduce P=24 Correctness passed!
- 
- 
-==================================
- 
-##Many2Many 15
-size_in_file: 15, size: 15
-Time taken by custom function: 11.418711
-Correct Output
- 
-##Many2Many 24
-size_in_file: 24, size: 24
-Time taken by custom function: 31.630223
-Correct Output
-==================== RUNTIME TESTS =====================
- 
-##AllReduce
-Custom function time: 22.021015
-Runtime test passed!
-Runtime test passed!
- 
-==================================
- 
-##Many2Many
-Custom function time: 31.538533
-Runtime test passed!
-Runtime test passed!
-(base) [hliu686@atl1-1-02-010-9-1 pa2]$ 
--- ProgramAssignments/PA_2/code/custom_collectives.cpp
+++ ProgramAssignments/PA_2/code/custom_collectives.cpp
@@ -7,14 +7,31 @@
 ////////////////////////////////////////////////////////////////
 // All Reduce (Hypercubic permutation method)
 void custom_allreduce_sum(int *local, int *global, int num_elem, int rank, int size) {
+  /*
+  Parameters:
+    local: the pointer to the local data
+    global: the pointer to the global data
+    num_elem: the number of elements in the data
+    rank: the rank of the process
+    size: the number of processes
+  */
+
   //// 1. Initialization ////
-  // create a buffer to store the received data
+  // create a local buffer to store the received data
   int* buffer = new int[num_elem];
 
   //// 2. Hypercubic permutation method ////
-  // transverse d = 0, 1, ..., ceil(log2(size))-1
-  for (int d = 0; (1 << d) < size; d++) {
-    int partner = rank ^ (1 << d);
+  /*
+  d                 = the number of dimensions
+  1 << d            := 2^d, a mask for ``flip operation''
+  rank ^ (1 << d)   := rank XOR 2^d, ``flip operation'' flip the d-th bit of rank
+  partner           = rank XOR 2^d,i the partner processpr of rank-th processor
+  for loop          : transverse d = 0, 1, ..., ceil(log2(size))-1, e.g. 2^3 = 8 => (0, 1, 2)
+  */
+  // for each dimention d
+  for (int d = 0; (1 << d) < size; d++) { 
+    // find the partner process for the current rank of the process
+    int partner = rank ^ (1 << d); 
 
     // only communicate with valid partners
     if (partner < size) {
@@ -25,12 +42,12 @@ void custom_allreduce_sum(int *local, int *global, int num_elem, int rank, int s
       */
       if (rank < partner) {
         // lower rank send first
-        MPI_Send(local, num_elem, MPI_INT, partner, d, MPI_COMM_WORLD);
+        MPI_Send(local,  num_elem, MPI_INT, partner, d, MPI_COMM_WORLD);
         MPI_Recv(buffer, num_elem, MPI_INT, partner, d, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       } else {
         // higher rank receive first
         MPI_Recv(buffer, num_elem, MPI_INT, partner, d, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Send(local, num_elem, MPI_INT, partner, d, MPI_COMM_WORLD);
+        MPI_Send(local,  num_elem, MPI_INT, partner, d, MPI_COMM_WORLD);
       }
       // conduct the sum operation
       for (int i = 0; i < num_elem; i++) {
@@ -38,7 +55,7 @@ void custom_allreduce_sum(int *local, int *global, int num_elem, int rank, int s
       }
     }
   }
-  // free the buffer
+  // free the loacl buffer
   delete[] buffer;
 
   //// 3. save the result to the global buffer ////
@@ -51,91 +68,160 @@ void custom_allreduce_sum(int *local, int *global, int num_elem, int rank, int s
 
 
 ////////////////////////////////////////////////////////////////////////////////
-// Many-to-one (Arbiturary permutation method)
 int custom_many2many(int *send_data, int *sendcounts, int** recv_data_ptr, int rank, int size) {
-  
-  //// 1. Initialization ////
-  // Swittch the number of data each process sends to others 
-  int* recvcounts = new int[size];
+  /*
+    Parameters:
+      - send_data: Pointer to the data array to be sent.
+      - sendcounts: Array of length 'size'. sendcounts[j] indicates the number of elements
+                     this process sends to process j.
+      - recv_data_ptr: Pointer to a pointer for the received data. This function will allocate
+                      the buffer for the received data and assign its address to *recv_data_ptr.
+      - rank: The rank of the current process.
+      - size: The total number of processes.
 
-  // copy the number of data to send to itself, saving extra communication resource
-  recvcounts[rank] = sendcounts[rank];
+    Return:
+        - The total number of elements received.
+   */
 
-  //// 2. Communication ////
-  // transverse all other processes
+  // STEP 0: Allocate memory for a full p x p matrix to hold all processes' sendcounts.
+  // The matrix is stored in row-major order, where row i contains the sendcounts of process i.
+  int* all_sendcounts = new int[size * size];
+  /*
+    Copy the sendcounts of the current process (its own row) into all_sendcounts.
+    For row corresponding to this process (rank), set:
+    all_sendcounts[rank * size + j] = number of elements this process sends to process j.
+  */
   for (int j = 0; j < size; j++) {
-    // skip itself
-    if (j == rank) continue; 
-    
-    // Avoid deadlock 
-    /*
-    Each process can either send or receive data to/from another process.
-    Therefore deadlock occurs when two processes in the pipeline trying to both send data to (or both receive data from) each other at the same time
-    */
-    if (rank < j) {
-      // ower rank send first
-      MPI_Send(&sendcounts[j], 1, MPI_INT, j, 100, MPI_COMM_WORLD);
-      MPI_Recv(&recvcounts[j], 1, MPI_INT, j, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    } else { // rank > j
-      // higher rank receive first
-      MPI_Recv(&recvcounts[j], 1, MPI_INT, j, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      MPI_Send(&sendcounts[j], 1, MPI_INT, j, 100, MPI_COMM_WORLD);
+    all_sendcounts[rank * size + j] = sendcounts[j];
+  }
+
+  // STEP 1: Exchange sendcounts with all other processes (manual allgather).
+  /*
+    Each process will exchange its row (sendcounts array) with every other process.
+    To avoid deadlock, if rank < i then send first then receive; otherwise, receive first then send.
+  */
+  for (int i = 0; i < size; i++) {
+    if (i == rank) continue; // Skip self
+    if (rank < i) {
+      // Lower-ranked process: send my row, then receive row from process i.
+      MPI_Send(all_sendcounts + rank * size, size, MPI_INT, i, 100, MPI_COMM_WORLD);
+      MPI_Recv(all_sendcounts + i * size,     size, MPI_INT, i, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    } else {
+      // Higher-ranked process: receive row from process i, then send my row.
+      MPI_Recv(all_sendcounts + i * size, size, MPI_INT, i, 100, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Send(all_sendcounts + rank * size, size, MPI_INT, i, 100, MPI_COMM_WORLD);
     }
   }
-  
-  //// 3. calculate the total length of received data and allocate the receive buffer ////
+
+  // STEP 2: Calculate the total number of elements to be received and the offset for each source.
+  /*
+      For the current process, the number of elements to receive from process i is:
+      recvcount_from[i] = all_sendcounts[i*size + rank]
+  */
+  int* recvcount_from = new int[size];
+  for (int i = 0; i < size; i++) {
+    recvcount_from[i] = all_sendcounts[i * size + rank];
+  }
+  // Compute the total number of elements to be received.
   int total_recv = 0;
   for (int i = 0; i < size; i++) {
-    total_recv += recvcounts[i];
+    total_recv += recvcount_from[i];
   }
-  int* recv_data = new int[total_recv];
+  // Allocate memory for the final receive buffer.
+  int* final_recv = new int[total_recv];
 
-  // calculate the starting position of received data in recv_data
-  int* displs = new int[size];
-  displs[0] = 0;
+  // Compute the receive offsets for each source process.
+  /*
+    recv_offset[i] indicates the starting index in final_recv for data received from process i.
+  */
+  int* recv_offset = new int[size];
+  recv_offset[0] = 0;
   for (int i = 1; i < size; i++) {
-    displs[i] = displs[i-1] + recvcounts[i-1];
+    recv_offset[i] = recv_offset[i - 1] + recvcount_from[i - 1];
+  }
+
+  // STEP 3: Compute the send offsets.
+  /*
+    send_offset[j] indicates the starting index in send_data for data that should be sent to process j.
+    send_offset[0] = 0, and send_offset[j] = send_offset[j-1] + sendcounts[j-1].
+  */
+  int* send_offset = new int[size];
+  send_offset[0] = 0;
+  for (int j = 1; j < size; j++) {
+    send_offset[j] = send_offset[j - 1] + sendcounts[j - 1];
   }
   
-  // calculate the starting position of send data in send_data
-  int* send_displs = new int[size];
-  send_displs[0] = 0;
-  for (int i = 1; i < size; i++) {
-    send_displs[i] = send_displs[i-1] + sendcounts[i-1];
-  }
-  
-  //// 4. Exchange the data between processes ////
-  for (int i = 0; i < size; i++) {
-    if (i == rank) { // slect diagonal elements
-      // copy the data from send_data to recv_data
-      for (int k = 0; k < sendcounts[rank]; k++) {
-        recv_data[displs[rank] + k] = send_data[send_displs[rank] + k];
-      }
-    } else {
-      // Avoid deadlock
+  // STEP 4: Exchange data with other processes using a ring-based approach with message splitting.
+  /*
+    For each round j (from 1 to size-1):
+      - Determine the destination:  dest   = (rank + j) mod size.
+      - Determine the source:       source = (rank - j + size) mod size.
+      - Instead of sending the entire message at once, split the message into smaller chunks.
+      - Divide the message size by 'size' (number of processes, P) to obtain a chunk size.
+  */
+  for (int j = 1; j < size; j++) {
+    int dest = (rank + j) % size;
+    int source = (rank - j + size) % size;
+
+    // Determine the number of elements to send to 'dest' and to receive from 'source'.
+    int send_count = all_sendcounts[rank * size + dest]; // Same as sendcounts[dest]
+    int recv_count = all_sendcounts[source * size + rank];
+
+    // Compute chunk sizes for sending and receiving.
+    /*
+      We divide the total message length by the number of processes to determine
+      Size of each block = m/P
+       (send_count + size - 1): - 1 is added to ensure that the division rounds up.
+       (send_count + size - 1) / size: This is equivalent to ceil(send_count / size).
+    */ 
+    int chunk_size_send = (send_count + size - 1) / size;
+    int chunk_size_recv = (recv_count + size - 1) / size;
+
+    // Define the number of steps (chunks) to iterate.
+    int steps = std::max(size, 1); // Ensure at least one step to avoid division by zero
+
+    // Loop over each chunk.
+    for (int c = 0; c < steps; c++) {
+      // Compute offset and length for the sending chunk.
+      int off_s = c * chunk_size_send;
+      if (off_s >= send_count) break; // No more data to send in this round.
+      int len_s = std::min(chunk_size_send, send_count - off_s);
+
+      // Compute offset and length for the receiving chunk.
+      int off_r = c * chunk_size_recv;
+      if (off_r >= recv_count) break; // No more data to receive.
+      int len_r = std::min(chunk_size_recv, recv_count - off_r);
+
+      // To avoid deadlock, use a convention:
       /*
-      Each process can either send or receive data to/from another process.
-      Therefore deadlock occurs when two processes in the pipeline trying to both send data to (or both receive data from) each other at the same time
+        if (rank < dest) then send first and then receive; else, receive first and then send.
       */
-      if (rank < i) {
-        // lower rank send first
-        MPI_Send(send_data + send_displs[i], sendcounts[i], MPI_INT, i, 200, MPI_COMM_WORLD);
-        MPI_Recv(recv_data + displs[i], recvcounts[i], MPI_INT, i, 200, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      } else { // rank > i
-        // higher rank receive first
-        MPI_Recv(recv_data + displs[i], recvcounts[i], MPI_INT, i, 200, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Send(send_data + send_displs[i], sendcounts[i], MPI_INT, i, 200, MPI_COMM_WORLD);
+      if (rank < dest) {
+        MPI_Send(send_data + send_offset[dest] + off_s, len_s, MPI_INT, dest, 200 + j, MPI_COMM_WORLD);
+        MPI_Recv(final_recv + recv_offset[source] + off_r, len_r, MPI_INT, source, 200 + j, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      } else {
+        MPI_Recv(final_recv + recv_offset[source] + off_r, len_r, MPI_INT, source, 200 + j, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(send_data + send_offset[dest] + off_s, len_s, MPI_INT, dest, 200 + j, MPI_COMM_WORLD);
       }
     }
   }
-  
-  //// 5. Post treatment ////
-  // free the memory 
-  delete[] recvcounts;
-  delete[] displs;
-  delete[] send_displs;
 
-  //  return the pointer to the received data
-  *recv_data_ptr = recv_data;
+  // STEP 5: Process the data that the process sends to itself.
+  /*
+    For j = 0, simply copy the data from send_data to final_recv.
+  */
+  int self_count = all_sendcounts[rank * size + rank]; // This equals sendcounts[rank]
+  for (int k = 0; k < self_count; k++) {
+    final_recv[recv_offset[rank] + k] = send_data[send_offset[rank] + k];
+  }
+
+  // STEP 6: Clean up temporary arrays and return the result.
+  delete[] all_sendcounts;
+  delete[] recvcount_from;
+  delete[] recv_offset;
+  delete[] send_offset;
+
+  *recv_data_ptr = final_recv;
   return total_recv;
 }
+
